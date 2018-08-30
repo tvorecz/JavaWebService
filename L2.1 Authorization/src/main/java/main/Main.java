7af6cbed959bb9bdc52d5128f44d167a47bc500b
Dmitry Zorich <tvorecz@gmail.com>
2018-08-30 20:20:13 +0300

    Done!

--- L2.1 Authorization/src/main/java/main/Main.java
+++ L2.1 Authorization/src/main/java/main/Main.java
@@ -9,6 +9,8 @@ import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import servlets.SessionsServlet;
+import servlets.SignInServlet;
+import servlets.SignUpServlet;
 import servlets.UsersServlet;
 
 /**
@@ -28,6 +30,8 @@ public class Main {
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.addServlet(new ServletHolder(new UsersServlet(accountService)), "/api/v1/users"); //path to work with users
         context.addServlet(new ServletHolder(new SessionsServlet(accountService)), "/api/v1/sessions"); //path to work with sessions
+        context.addServlet(new ServletHolder(new SignInServlet(accountService)), "/signin");
+        context.addServlet(new ServletHolder(new SignUpServlet(accountService)), "/signup");
 
         ResourceHandler resource_handler = new ResourceHandler();
         resource_handler.setResourceBase("public_html");
@@ -39,6 +43,7 @@ public class Main {
         server.setHandler(handlers);
 
         server.start();
+        System.out.println("Server started");
         server.join();
     }
 }
